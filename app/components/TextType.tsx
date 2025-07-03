071fa1d5f63969738f94f341e0dcc6ac4b734222
joaoleite2 <joao.cont3t@gmail.com>
2025-07-03 20:22:23 -0300

    feat: text type

--- app/components/TextType.tsx
+++ app/components/TextType.tsx
@@ -1,7 +1,9 @@
+import Card from './ui/Card'
+
 const TextType = () => {
   return (
     <section className='flex flex-col justify-center items-center mt-5'>
-      <h1>Tipo de texto</h1>
+      <Card />
     </section>
   )
 }
---
+++ app/components/ui/Card.tsx
@@ -0,0 +1,10 @@
+const Card = () => {
+  return (
+    <div className='bg-red-200 p-4 rounded-3xl w-[300px] h-[300px]'>
+      <h1>Card</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+    </div>
+  )
+}
+
+export default Card
