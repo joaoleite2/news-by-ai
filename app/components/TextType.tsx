8ed16523be5cb0e2e8ed7af0fc01ca32c747eefd
joaoleite2 <joao.cont3t@gmail.com>
2025-07-03 20:55:25 -0300

    feat: text type cards is ready

--- app/components/TextType.tsx
+++ app/components/TextType.tsx
@@ -1,9 +1,35 @@
+import { Library, ListCollapse, Zap } from 'lucide-react'
 import Card from './ui/Card'
+
+const cards = [
+  {
+    title: 'Leitura Rápida',
+    description: 'Receba notícias em formato de leitura rápida, com resumos e informações essenciais.',
+    icon: <Zap fill='currentColor' size={30} className='text-amber-500' />,
+    color: 'amber'
+  },
+  {
+    title: 'Leitura Detalhada',
+    description: 'Receba notícias em formato de leitura detalhada, com informações completas e detalhes.',
+    icon: <Library fill='currentColor' size={30} className='text-blue-500' />,
+    color: 'blue'
+  },
+  {
+    title: 'Pontos-Chave ',
+    description: 'Receba notícias em formato de pontos-chave, com informações essenciais e detalhadas.',
+    icon: <ListCollapse fill='currentColor'  size={30} className='text-red-500' />,
+    color: 'red'
+  }
+]
 
 const TextType = () => {
   return (
     <section className='flex flex-col justify-center items-center mt-5'>
-      <Card />
+      <div className='flex gap-4'>
+        {cards.map((card) => (
+          <Card key={card.title} title={card.title} description={card.description} icon={card.icon} color={card.color} />
+        ))}
+      </div>
     </section>
   )
 }
--- app/components/ui/Card.tsx
+++ app/components/ui/Card.tsx
@@ -1,8 +1,30 @@
-const Card = () => {
+interface CardProps {
+  title: string
+  description: string
+  icon: React.ReactNode
+  color?: string
+}
+
+const Card = ({ title, description, icon, color }: CardProps) => {
+  const colors = {
+    'amber': 'hover:border-amber-500',
+    'blue': 'hover:border-blue-500',
+    'red': 'hover:border-red-500'
+  }
+  
   return (
-    <div className='bg-red-200 p-4 rounded-3xl w-[300px] h-[300px]'>
-      <h1>Card</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+    <div className={`
+      cursor-pointer flex flex-col bg-white border-2 border-gray-200 p-4 rounded-3xl
+      w-[250px] h-[280px] transition-all duration-300 ${colors[color as keyof typeof colors] || ''}
+      hover:scale-103`}
+    >
+      <h1 className='font-bold text-2xl text-gray-800'>{title}</h1>
+      <p className='text-gray-500 text-md mt-2'>
+        {description}
+      </p>
+      <div className='flex-1 flex justify-end items-end'>
+        {icon}
+      </div>
     </div>
   )
 }
